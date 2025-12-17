69ba3c5e3d56d25d30538da981110b82b506c58c
Jawad-14 <jawadashraf1235@gmail.com>
2025-12-17 01:03:49 -0800

    updated

--- salary mangement.cpp
+++ salary mangement.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-#include<string>
+#include <string>
 using namespace std;
 
 struct Employee {
     int id;
     string name;
+    string fatherName;
+    string cnic;
+    string dateOfJoining;
     double basicSalary;
     char grade;
     string department;
     double allowance;
+    double tax;
     double netSalary;
-    int tax;
 };
 
 Employee emp[100];
@@ -19,26 +22,36 @@ int countEmp = 0;
 void addEmployee() {
     cout << "\nEnter Employee ID: ";
     cin >> emp[countEmp].id;
+
     cout << "Enter Name: ";
     cin >> emp[countEmp].name;
+
+    cout << "Enter Father Name: ";
+    cin >> emp[countEmp].fatherName;
+
+    cout << "Enter CNIC (13 digits without using '-'): ";
+    cin >> emp[countEmp].cnic;
+
+    cout << "Enter Date of Joining(mm-dd-yyyy): ";
+    cin >> emp[countEmp].dateOfJoining;
+
     cout << "Enter Basic Salary: ";
     cin >> emp[countEmp].basicSalary;
+
     cout << "Enter Grade: ";
     cin >> emp[countEmp].grade;
-     cout << "Enter Allowance: ";
+
+    cout << "Enter Allowance: ";
     cin >> emp[countEmp].allowance;
 
     cout << "Enter Department: ";
     cin >> emp[countEmp].department;
 
-    double basic = emp[countEmp].basicSalary;
-    
-   int tax = basic * 0.05; 
-
-    emp[countEmp].netSalary = basic + emp[countEmp].allowance - tax;
+    emp[countEmp].tax = emp[countEmp].basicSalary * 0.05;
+    emp[countEmp].netSalary = emp[countEmp].basicSalary + emp[countEmp].allowance - emp[countEmp].tax;
 
     countEmp++;
-    cout << "\nEmployee Added Successfully !\n";
+    cout << "\nEmployee Added Successfully!\n";
 }
 
 void displayEmployees() {
@@ -51,12 +64,15 @@ void displayEmployees() {
     for (int i = 0; i < countEmp; i++) {
         cout << "\nID: " << emp[i].id;
         cout << "\nName: " << emp[i].name;
+        cout << "\nFather Name: " << emp[i].fatherName;
+        cout << "\nCNIC: " << emp[i].cnic;
+        cout << "\nDate of Joining: " << emp[i].dateOfJoining;
         cout << "\nBasic Salary: " << emp[i].basicSalary;
         cout << "\nGrade: " << emp[i].grade;
         cout << "\nAllowance: " << emp[i].allowance;
+        cout << "\nTax (5%): " << emp[i].tax;
         cout << "\nNet Salary: " << emp[i].netSalary;
         cout << "\nDepartment: " << emp[i].department;
-         
         cout << "\n------------------------";
     }
 }
@@ -69,12 +85,17 @@ void searchEmployee() {
     for (int i = 0; i < countEmp; i++) {
         if (emp[i].id == id) {
             cout << "\nRecord Found\n";
-            cout << "ID: " << emp[i].id;
+            cout << "\nID: " << emp[i].id;
             cout << "\nName: " << emp[i].name;
+            cout << "\nFather Name: " << emp[i].fatherName;
+            cout << "\nCNIC: " << emp[i].cnic;
+            cout << "\nDate of Joining: " << emp[i].dateOfJoining;
             cout << "\nBasic Salary: " << emp[i].basicSalary;
             cout << "\nGrade: " << emp[i].grade;
             cout << "\nAllowance: " << emp[i].allowance;
+            cout << "\nTax (5%): " << emp[i].tax;
             cout << "\nNet Salary: " << emp[i].netSalary;
+            cout << "\nDepartment: " << emp[i].department;
             return;
         }
     }
@@ -108,21 +129,20 @@ void updateEmployee() {
         if (emp[i].id == id) {
             cout << "Enter New Basic Salary: ";
             cin >> emp[i].basicSalary;
+
             cout << "Enter New Grade: ";
             cin >> emp[i].grade;
+
             cout << "Enter New Department: ";
             cin >> emp[i].department;
+
             cout << "Enter New Allowance: ";
-    cin >> emp[countEmp].allowance;
-    
+            cin >> emp[i].allowance;
 
-            double basic = emp[i].basicSalary;
-            
-            double tax = basic * 0.05;
+            emp[i].tax = emp[i].basicSalary * 0.05;
+            emp[i].netSalary = emp[i].basicSalary + emp[i].allowance - emp[i].tax;
 
-            emp[i].netSalary = basic + emp[i].allowance - tax;
-
-            cout << "\nRecord Updated !\n";
+            cout << "\nRecord Updated!\n";
             return;
         }
     }
@@ -143,7 +163,7 @@ int main() {
         cout << "Enter Your Choice: ";
         cin >> choice;
 
-        switch(choice) {
+        switch (choice) {
             case 1: addEmployee(); break;
             case 2: displayEmployees(); break;
             case 3: searchEmployee(); break;
